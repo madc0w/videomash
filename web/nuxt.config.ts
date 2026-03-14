da1fab4c46dc9df64b5f9daa2cca877c8d6af746
Marc A. Donis <marc@balloonary.io>
2026-03-14 21:03:52 +0100

    .

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			],
+			link: [
+				{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+			],
 		},
 	},
 	compatibilityDate: '2025-01-01',
